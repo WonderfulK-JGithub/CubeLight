698ad5d16896bb8b0c4358bcbc053df8f0f86318
WonderfulK-JGithub <70464858+WonderfulK-JGithub@users.noreply.github.com>
2020-11-08 09:27:06 +0100

    börjat med Fade Transition

---
+++ Kub-Ljus/Assets/FadeTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTransition : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.Play("FadeOut_TransitionEnter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
